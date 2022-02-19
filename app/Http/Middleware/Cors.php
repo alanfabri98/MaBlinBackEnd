dc45e821943476c6afffd17dff43e71a2532b115
alan oña <afona2@espe.edu.ec>
2022-02-18 18:13:00 -0600

    Deploy cambio de cors v4

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-               ->header('Access-Control-Allow-Origin', '*')
+               ->header('Access-Control-Allow-Origin', 'http://localhost:8100')
                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
                ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application,X-Token-Auth');
     }
