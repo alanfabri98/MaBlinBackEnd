b5a1a7fc030e0f84b850270f344dd015537c2925
alan oña <afona2@espe.edu.ec>
2022-02-28 19:01:28 -0600

    Implementacion del login version beta

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -13,16 +13,16 @@ class EstudianteController extends Controller
         $this->middleware('auth:api');
     }
 
-    public function index()
-    {
-        return response()->json(Estudiante::all(), 200);
-    }
+    //public function index()
+    //{
+    //    return response()->json(Estudiante::all(), 200);
+    //}
 
     public function store(Request $request)
     {
-        
-        $estudiante = $request->all();
         $us = auth()->user();
+
+        $estudiante = $request->all();
         
         $estudiante['n'] = $request->n;
         $estudiante['a'] = $request->a;
@@ -31,50 +31,32 @@ class EstudianteController extends Controller
 
         $newEstudiante = Estudiante::create($estudiante);
         return response()->json($newEstudiante, 201);
-        /*
-        $us = auth()->user();
-        if($us->ro == "Admin"){
-            $estudiante = Estudiante::create($request->all());
-            return response()->json($estudiante, 201);
-        }else{
-            return response()->json(['error' => 'El rol no tiene permisos'], 403);
-        }
-        */
     }
 
     public function show(){
         $us = auth()->user();
         $estudiantes = Estudiante::where("i","=", $us->id)->get();
-        return response()->json($estudiantes, 200);//Empleado::where('id', 1)->get();
-        //return response()->json(Estudiante::all(), 200);
+        return response()->json($estudiantes, 200);
     }
 
     public function update(Request $request){
-        $us = auth()->user();
-        if($us->ro == "Admin"){
-            $estudiante = Estudiante::findOrFail($request->id);
-            $estudiante->n = $request->n;
-            $estudiante->a = $request->a;
-            $estudiante->f = $request->f;
-            $estudiante->i = $request->i;
-            $estudiante->save();
-            return response()->json($estudiante, 200);
-        }else{
-            return response()->json(['error' => 'El rol no tiene permisos'], 403);
-        }
+        $estudiante = Estudiante::findOrFail($request->id);
+        $estudiante->n = $request->n;
+        $estudiante->a = $request->a;
+        $estudiante->f = $request->f;
+        $estudiante->i = $request->i;
+        $estudiante->save();
+        return response()->json($estudiante, 200);
+        //$us = auth()->user();
+        //if($us->ro == "Admin"){
+        //    
+        //}else{
+        //    return response()->json(['error' => 'El rol no tiene permisos'], 403);
+        //}
     }
     
     public function delete(string $request){
         Estudiante::destroy((int)$request);
         return response()->json('Borrado', 204);
-        /*
-        $us = auth()->user();
-        if($us->ro == "Admin"){
-            Estudiante::destroy((int)$request);
-            return response()->json('Borrado', 204);
-        }else{
-            return response()->json(['error' => 'El rol no tiene permisos'], 403);
-        }
-        */
     }
 }
